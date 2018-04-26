dac70af2152c5197c888a50050bb67e1659f1885
chenxuelian <chenxuelian@51shebao.com>
2018-04-26 10:38:36 +0800

    table组件

---
+++ src/components/table/table.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="yu-table"
+    :style="{'text-align':textPos}"
+    >
+        <table cellspacing="0">
+            <thead>
+                <tr class="yu-th">
+                    <yuTd v-for="item in title" :data='item.tit' :border='border'></yuTd>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in data">
+                    <yuTd :data='item.name' :border='border'></yuTd>
+                    <yuTd :data='item.date' :border='border'></yuTd>
+                    <yuTd :data='item.address' :border='border'></yuTd>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import yuTd from './td'
+export default {
+   name: 'yu-table',
+   components:{
+      yuTd
+  }, 
+    props:{
+        height:{
+            type:String,
+            default:''
+        },
+        maxHeight:{
+            type:String,
+            default:''
+        },
+        border:{//是否带边框
+            type:Boolean,
+            default:false
+        },
+        textPos:{//文字位置
+            type:String,
+            default:'left'
+        }
+        
+    },
+    data(){
+      return {
+          data:[{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }],
+          title:[
+              {tit:'姓名'},
+              {tit:'日期'},
+              {tit:'地区'}
+              ]
+          
+      }
+    },
+    methods:{
+       
+    },
+    computed:{
+
+    },
+    //  created(){
+    //    console.log(this.border)
+    // },
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .yu-table,table{
+        width: 100%;  
+        border-collapse:collapse;
+    }
+    
+    th,td{
+        font-weight: normal;     
+        padding: 8px 0 8px 10px;
+        border-bottom: 1px solid #e1e7f6;
+        position: relative;
+        line-height: 24px;
+        max-width: 160px;
+        font-size: 14px;
+        color: #666;
+    }
+    .yu-th{
+        background-color: #F4F5F9;
+        font-weight: normal;
+        width: auto;
+    }
+    tr:hover{
+        background-color: #F4F5F9;
+    }
+</style>
---
+++ src/components/table/td.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="yu-td" :class="{'yu-border':border}">
+       {{ data }}
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'yuTd',
+    props:{
+       data:{
+           type:String,
+           default:''
+       },
+       border:{
+           type:Boolean,
+            default:true
+       }
+        
+    },
+    data(){
+      return {
+          
+      }
+    },
+    methods:{
+       
+    },
+    computed:{
+
+    },
+    created(){
+       
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .yu-td{
+        display: table-cell;
+        vertical-align: inherit;
+        font-weight: normal;     
+        padding: 8px 0 8px 10px;
+        border-bottom: 1px solid #e1e7f6;
+        position: relative;
+        line-height: 24px;
+        max-width: 160px;
+        font-size: 14px;
+        color: #666;
+        word-break: break-all;
+        word-wrap: break-word;
+    }
+    .yu-border{
+        border: 1px solid #e1e7f6;
+    }
+</style>
